fe36447324b9598b1688965a9faf86fd96e45d1b
Ne0nx3r0 <ne0nx3r0@gmail.com>
2012-12-21 14:41:44 -0600

    updating description

--- com/gmail/ne0nx3r0/coolpoints/CoolPoints.java
+++ com/gmail/ne0nx3r0/coolpoints/CoolPoints.java
@@ -3,6 +3,7 @@ package com.gmail.ne0nx3r0.coolpoints;
 import com.gmail.ne0nx3r0.coolpoints.commands.CoolPointsCommandExecutor;
 import com.gmail.ne0nx3r0.coolpoints.listeners.CoolPointsPlayerListener;
 import com.gmail.ne0nx3r0.coolpoints.points.PlayerProfileManager;
+import lib.PatPeter.SQLibrary.SQLite;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -10,11 +11,53 @@ public class CoolPoints extends JavaPlugin
 {
     public static Plugin self;
     public static PlayerProfileManager ppm;
+    public static SQLite sqlite;
 
     @Override
     public void onEnable()
     {        
         CoolPoints.self = this;
+
+// DB Setup
+        sqlite = new SQLite(
+            getLogger(),
+            "CoolPoints",
+            "points",
+            this.getDataFolder().getAbsolutePath()
+        );
+        
+        try 
+        {
+            sqlite.open();
+        }
+        catch(Exception e)
+        {
+            getLogger().info(e.getMessage());
+            getPluginLoader().disablePlugin(this);
+        }
+        
+        if(sqlite.checkTable("player"))
+        {
+            return;
+        }
+        else
+        {
+            sqlite.query("CREATE TABLE player("
+                + "username VARCHAR(16) PRIMARY KEY,"
+                + "points INT,"
+                + "giftedToday BOOLEAN,"
+                + "firstJoined INT"
+            + ");");
+            
+            sqlite.query("CREATE TABLE points_given("
+                + "created INT PRIMARY KEY,"
+                + "fromName VARCHAR(16) PRIMARY KEY,"
+                + "toName VARCHAR(16) PRIMARY KEY,"
+                + "amount INT"
+            + ");");
+
+            getLogger().info("Cool Points tables created.");
+        }
         
         ppm = new PlayerProfileManager();
         
@@ -29,5 +72,6 @@ public class CoolPoints extends JavaPlugin
     public void onDisable()
     {      
         ppm.saveAllPlayerProfiles();
+        sqlite.close();
     }
 }
--- com/gmail/ne0nx3r0/coolpoints/points/PlayerProfileManager.java
+++ com/gmail/ne0nx3r0/coolpoints/points/PlayerProfileManager.java
@@ -2,56 +2,50 @@ package com.gmail.ne0nx3r0.coolpoints.points;
 
 import com.gmail.ne0nx3r0.coolpoints.CoolPoints;
 import java.io.File;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.logging.Level;
 import java.util.logging.Logger;
-import org.bukkit.configuration.file.FileConfiguration;
-import org.bukkit.configuration.file.YamlConfiguration;
 
 public class PlayerProfileManager
 {
-    private final File PROFILE_DIRECTORY;
-    
     public PlayerProfileManager()
     {
-        PROFILE_DIRECTORY = new File(CoolPoints.self.getDataFolder(),"players");
+        File PLUGIN_DIR = new File(CoolPoints.self.getDataFolder().getPath());
         
-        if(!PROFILE_DIRECTORY.exists())
+        if(!PLUGIN_DIR.exists())
         {
-            PROFILE_DIRECTORY.mkdirs();
-        }
-    }
-
-    public void unloadProfile(String sPlayerName)
-    {
-        String sPlayerNameLower = sPlayerName.toLowerCase();
-        if(playerProfiles.containsKey(sPlayerNameLower))
-        {
-            saveProfile(sPlayerNameLower);
-            
-            playerProfiles.remove(sPlayerNameLower);
-        }
-    }
-
-    public void saveAllPlayerProfiles()
-    {
-        for(String sPlayerNameLower : playerProfiles.keySet())
-        {
-            saveProfile(sPlayerNameLower);
+            PLUGIN_DIR.mkdirs();
         }
     }
 
     public int getCoolPoints(String sPlayerName)
     {
-        String sPlayerNameToLower = sPlayerName.toLowerCase();
-        
-        if(playerProfiles.containsKey(sPlayerNameToLower))
+        try
         {
-            return playerProfiles.get(sPlayerNameToLower).getPoints();
+            PreparedStatement statement = CoolPoints.sqlite.prepare(""
+                + "SELECT points "
+                + "FROM players "
+                + "WHERE username=? LIMIT 1;");
+            
+            statement.setString(1, sPlayerName.toLowerCase());
+            
+            ResultSet result = statement.executeQuery();
+            
+            if(result != null)
+            {
+                return result.getInt("points");
+            }
+            else
+            {
+                return -1;
+            }
         }
+        catch (Exception ex)
+        {
+            Logger.getLogger(PlayerProfileManager.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
         
         return -1;
     }
