dae61ff3b36605b31c1b489215f0dbe322c14ed4
Ne0n x3r0 <ne0nx3r0@gmail.com>
2013-02-07 13:12:08 -0600

    Cleaning things up for a DBO release.

--- com/gmail/ne0nx3r0/coolpoints/CoolPoints.java
+++ com/gmail/ne0nx3r0/coolpoints/CoolPoints.java
@@ -3,9 +3,14 @@ package com.gmail.ne0nx3r0.coolpoints;
 import com.gmail.ne0nx3r0.coolpoints.commands.CoolPointsCommandExecutor;
 import com.gmail.ne0nx3r0.coolpoints.listeners.CoolPointsPlayerListener;
 import com.gmail.ne0nx3r0.coolpoints.points.PlayerProfileManager;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.Calendar;
 import java.util.logging.Level;
 import lib.PatPeter.SQLibrary.SQLite;
+import net.h31ix.updater.Updater;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -15,12 +20,22 @@ public class CoolPoints extends JavaPlugin
     public static Plugin self;
     public static PlayerProfileManager ppm;
     public static SQLite sqlite;
+    public boolean UPDATE_AVAILABLE = false;
+    public String UPDATE_NAME;
 
     @Override
     public void onEnable()
     {        
         CoolPoints.self = this;
-
+        
+// Load config
+        File configFile = new File(this.getDataFolder(), "config.yml");   
+        
+        if(!configFile.exists()){
+            configFile.getParentFile().mkdirs();
+            copy(this.getResource(configFile.getName()), configFile);
+        }        
+        
 // DB Setup
         sqlite = new SQLite(
             getLogger(),
@@ -61,19 +76,21 @@ public class CoolPoints extends JavaPlugin
         
         ppm = new PlayerProfileManager();
         
-        //register events
-        getServer().getPluginManager().registerEvents(new CoolPointsPlayerListener(), this);
+//register events
+        getServer().getPluginManager().registerEvents(new CoolPointsPlayerListener(this), this);
         
-        //register commands
+//register commands
         getCommand("cp").setExecutor(new CoolPointsCommandExecutor());
         
-        //Setup daily point reset thingy
+//Setup daily point reset thingy
         this.getServer().getScheduler().scheduleSyncRepeatingTask(self,new Runnable()
         {
             @Override 
             public void run()
             {      
-                if(Calendar.HOUR_OF_DAY == 0)
+                System.out.println("Hour was:"+Calendar.getInstance().HOUR_OF_DAY);
+                
+                if(Calendar.getInstance().HOUR_OF_DAY == 0)
                 {
                     Bukkit.broadcastMessage("[SERVER] Resetting CoolPoint daily stats!");
 
@@ -82,11 +99,49 @@ public class CoolPoints extends JavaPlugin
             }
         }, 
         20*60*60,20*60*60);
+        
+        
+        if(getConfig().getBoolean("update-notifications"))
+        {
+            Updater updater = new Updater(
+                    this,
+                    "coolpoints",
+                    this.getFile(),
+                    Updater.UpdateType.NO_DOWNLOAD,
+                    false); // Start Updater but just do a version check
+
+            UPDATE_AVAILABLE = updater.getResult() == Updater.UpdateResult.UPDATE_AVAILABLE;
+            
+            if(UPDATE_AVAILABLE)
+            {
+                UPDATE_NAME = updater.getLatestVersionString();
+                
+                getLogger().log(Level.INFO,"--------------------------------");
+                getLogger().log(Level.INFO,"    An update is available:");
+                getLogger().log(Level.INFO,"    "+UPDATE_NAME);
+                getLogger().log(Level.INFO,"--------------------------------");
+            }
+        }
     }
 
     @Override
     public void onDisable()
     {
         sqlite.close();
+    }
+    
+    private void copy(InputStream in, File file) {
+        try {
+            OutputStream out = new FileOutputStream(file);
+            byte[] buf = new byte[1024];
+            int len;
+            while((len=in.read(buf))>0){
+                out.write(buf,0,len);
+            }
+            out.close();
+            in.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
--- com/gmail/ne0nx3r0/coolpoints/listeners/CoolPointsPlayerListener.java
+++ com/gmail/ne0nx3r0/coolpoints/listeners/CoolPointsPlayerListener.java
@@ -1,27 +1,34 @@
 package com.gmail.ne0nx3r0.coolpoints.listeners;
 
 import com.gmail.ne0nx3r0.coolpoints.CoolPoints;
+import org.bukkit.ChatColor;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 
 public class CoolPointsPlayerListener implements Listener
-{
+{    
+    private final CoolPoints plugin;
     
-    @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
-    public void onPlayerInteract(PlayerInteractEvent e)
+    public CoolPointsPlayerListener(CoolPoints plugin)
     {
-        
+        this.plugin = plugin;
     }
     
     @EventHandler(priority=EventPriority.NORMAL, ignoreCancelled=true)
     public void onPlayerJoin(PlayerJoinEvent e)
     {
-        if(CoolPoints.ppm.giveDailyWage(e.getPlayer().getName()))
+        if(e.getPlayer().hasPermission("coolpoints.earn")
+        && CoolPoints.ppm.giveDailyWage(e.getPlayer().getName()))
         {
             e.getPlayer().sendMessage("You have earned a cool point for logging in today!");
+        }
+        
+        if(plugin.UPDATE_AVAILABLE && e.getPlayer().hasPermission("coolpoints.manage"))
+        {
+            e.getPlayer().sendMessage(ChatColor.RED+"An update is available:"
+                +ChatColor.WHITE+plugin.UPDATE_NAME);
         }
     }
 }
